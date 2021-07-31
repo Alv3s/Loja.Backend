a8ef8aef306a341a2e1bef04565cc1bf11dd3e71
Alv3s <42544369+Alv3s@users.noreply.github.com>
2021-07-31 11:47:52 +0100

    Added DBObjects Project

---
+++ Loja.Backend.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using Loja.Backend.DBObjects;
+using System;
+using System.Collections.Generic;
+
+namespace Loja.Backend.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product();
+            product.Id = 10;
+            product.Name = "Nome Teste";
+            product.Description = "Descrição Teste";
+            product.Price = 10;
+
+            List<Product> products = new List<Product>();
+            products.Add(product);
+            products.Add(new Product()
+            {
+                Id = 10,
+                Name = "Nome Teste 2",
+                Description = "Descrição Teste 2",
+                Price = 10,
+            });
+
+            foreach (Product p in products)
+                Console.WriteLine(p);
+        }
+    }
+}
